eaf9515e0e9edd41fb172be2b61a5fae2be559d1
unknown <mohseni.amir.eh@gmail.com>
2024-05-19 02:44:41 -0700

    events can be add and recoreds will be save for future actions

--- js/index.js
+++ js/index.js
@@ -1,47 +1,37 @@
 'use strict'
 
-import { monthsName } from "./js/variables.js";
+import { monthsName } from "./variables.js";
 
 let selectedTime
+
+let allDayNodes
+let reserveSelected
+let eventArray =[
+    // {
+    //     date:'5/17/2024',
+    //     text:'some text'
+    // }
+]
 
 let firstDay
 let lastDay
 
 let tableBody = document.getElementById('tableBody')
 let currentDate = document.getElementById('currentDate')
+let textArea = document.getElementById('eventTextArea')
+let eventBox = document.getElementById('eventContainer')
 
-async function setSelectedTime(time = new Date().toLocaleDateString()){
-    console.log('time',time)
-    // if(time){
-        selectedTime = new Date(time);
-
-    // }else{
-        // selectedTime = new Date();
-    // }
-    // console.log('selectedTime',selectedTime)
-    selectedDayDetails(selectedTime)
+async function setSelectedTime(time = new Date().toLocaleDateString(), render = true){
+    selectedTime = new Date(time);
+    if(render)
+        selectedDayDetails(selectedTime)
 }
 
-function selectedDayDetails(a){
+async function selectedDayDetails(a){
     // 5/17/2024
-    // console.log(a)
-    
-    // const temp = new Date(a)
-    // dayNumber = temp.getDate()
-    // monthNumber = temp.getMonth()
-    // yearNumber = temp.getFullYear()
 
-    // console.log('dayNumber', dayNumber)
-    // console.log('monthNumber', monthNumber)
-    // console.log('yearNumber', yearNumber)
-    
     firstDay = new Date(selectedTime.getFullYear(), selectedTime.getMonth(), 1).getDay();
     lastDay = new Date(selectedTime.getFullYear(), selectedTime.getMonth() + 1, 0).getDay();
-    
-    
-    // console.log('d ',a,dayNumber, monthNumber,yearNumber)
-    // console.log('firstDay ',firstDay)
-    // console.log('lastDay ',lastDay)
 
     makeEachMonthComponent()
 }
@@ -55,14 +45,18 @@ function makeEachMonthComponent(){
         
     let tr = document.createElement('tr')
         
-    for(let i = 0; i <= 30 + firstDay; i++){
-        let td = document.createElement('td')
+    for(let i = 0; i < 30 + firstDay; i++){
+        let td
         if(i%7 == 0){
             tr = document.createElement('tr')
         }
         if(firstDay <= i){
+            td = document.createElement('td')
+            td.classList.add('box')
             td.innerText = i - firstDay + 1
         }else{
+            td = document.createElement('td')
+            td.classList.add('invisBox')
             td.innerText = ''
         }
         tr.appendChild(td)
@@ -70,7 +64,8 @@ function makeEachMonthComponent(){
     }
     let temp = new Date(selectedTime)
     console.log('temp',temp)
-    currentDate.innerText = temp.getMonth() + "/" + temp.getDate() + "/" + temp.getFullYear()
+    currentDate.innerText = monthsName[temp.getMonth()] + "/" + temp.getDate() + "/" + temp.getFullYear()
+    addClickEventToEachDay()
 }
 
 async function nextMonth(){
@@ -96,12 +91,36 @@ document.getElementById('prevBtn').addEventListener('click',() => {
 })
 
 
-function dateFace1(a,b,c){
-    return a + "/" + b + "/" + c
-}
-
-
-
-
 setSelectedTime()
 
+function addClickEventToEachDay(){
+    allDayNodes = document.querySelectorAll('.box')
+    allDayNodes.forEach((element, index)=> {
+        element.addEventListener('click', () => {
+            toggleShow()
+            let temp = selectedTime.getMonth() + `/${index+1}/` + selectedTime.getFullYear()
+            reserveSelected = temp
+            setSelectedTime(temp, false)
+        })
+    })
+}
+
+function toggleShow(){
+    eventBox.classList.toggle('event-box-show')
+}
+document.getElementById('closeModal').addEventListener('click',() => {
+    toggleShow()
+})
+
+document.getElementById('saveEvent').addEventListener('click',() => {
+    let temp = {
+        date: reserveSelected,
+        text: textArea.value
+    }
+    eventArray.push(temp)
+
+    console.log('array',eventArray)
+
+    setSelectedTime(reserveSelected)
+    toggleShow()
+})
