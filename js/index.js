a002b77c22a5319440973ff490c0b331fb6edf50
unknown <mohseni.amir.eh@gmail.com>
2024-05-21 00:44:11 -0700

    all component now work with one Date boject called selectedTime, and event now load in textarea

--- js/index.js
+++ js/index.js
@@ -23,11 +23,15 @@ let eventBox = document.getElementById('eventContainer')
 
 async function setSelectedTime(time = new Date().toLocaleDateString(), render = true){
     selectedTime = new Date(time);
-    if(render)
-        selectedDayDetails(selectedTime)
+    // console.log('set()',selectedTime.toLocaleDateString())
+    if(render){
+        console.log('1111', selectedTime.toLocaleDateString())
+        selectedDayDetails()
+    }
+    console.error('selected',selectedTime)
 }
 
-async function selectedDayDetails(a){
+function selectedDayDetails(a){
     // 5/17/2024
 
     firstDay = new Date(selectedTime.getFullYear(), selectedTime.getMonth(), 1).getDay();
@@ -36,7 +40,12 @@ async function selectedDayDetails(a){
     makeEachMonthComponent()
 }
 
+// async checkEventAvailablity(dateString){
+
+// }
 function makeEachMonthComponent(){
+    // console.log('time in makeCMP', selectedTime)
+    // remove all nodes to generate now
     if(tableBody.children.length > 0){
         while (tableBody.firstChild) {
             tableBody.firstChild.remove()
@@ -47,12 +56,23 @@ function makeEachMonthComponent(){
         
     for(let i = 0; i < 30 + firstDay; i++){
         let td
+
         if(i%7 == 0){
             tr = document.createElement('tr')
         }
         if(firstDay <= i){
+            let timeTemp = `${selectedTime.getMonth()+1}` + `/${i - firstDay + 1}/` + selectedTime.getFullYear()
+            // setSelectedTime(timeTemp, false)
             td = document.createElement('td')
+
+            eventArray.forEach(x => {
+                if(x.date === timeTemp){
+                    td.classList.add('event')
+                }
+            })
             td.classList.add('box')
+            if(selectedTime.getMonth())
+            // if(new Date(selectedTime).toLocaleDateString() == )
             td.innerText = i - firstDay + 1
         }else{
             td = document.createElement('td')
@@ -62,18 +82,35 @@ function makeEachMonthComponent(){
         tr.appendChild(td)
         tableBody.appendChild(tr)
     }
-    let temp = new Date(selectedTime)
-    console.log('temp',temp)
-    currentDate.innerText = monthsName[temp.getMonth()] + "/" + temp.getDate() + "/" + temp.getFullYear()
+
+    // renew the value of selectedTime to show
+    // setSelectedTime(new Date().toLocaleDateString(), false)
+    currentDate.innerText = monthsName[selectedTime.getMonth()] + "/" + selectedTime.getDate() + "/" + selectedTime.getFullYear()
     addClickEventToEachDay()
 }
 
 async function nextMonth(){
-    if(selectedTime.getMonth()+2 > 12){
-        setSelectedTime(1 + "/" + selectedTime.getDate() + "/" + selectedTime.getFullYear())
+    if(selectedTime.getMonth() >= 11){
+        selectedTime.setMonth(0)
+        selectedTime.setFullYear(selectedTime.getFullYear() + 1)
     }else{
-        setSelectedTime(selectedTime.getMonth()+2 + "/" + selectedTime.getDate() + "/" + selectedTime.getFullYear())
+        // let temp = selectedTime.getMonth() + 1
+        // console.log('selectedTime',selectedTime.toLocaleDateString())
+        // console.log('temp',temp.toString())
+        // selectedTime.setMonth(temp.toString())
+        // console.log('selectedTime',selectedTime.toLocaleDateString())
+        console.log('selectedTime',selectedTime.toLocaleDateString())
+        // let time = selectedTime.getMonth()+3 + '/' + selectedTime.getDate() + '/' + selectedTime.getFullYear()
+        // console.log('time',time)
+        selectedTime.setMonth((selectedTime.getMonth() + 1).toString())
+        // setSelectedTime(time)
     }
+    console.log('selectedTime',selectedTime.toLocaleDateString())
+    // setTimeout(() => {
+        setSelectedTime(selectedTime)
+    // },300) 
+    console.log('selectedTime',selectedTime.toLocaleDateString())
+
 }
 document.getElementById('nextBtn').addEventListener('click',() => {
     nextMonth()
@@ -81,26 +118,34 @@ document.getElementById('nextBtn').addEventListener('click',() => {
 
 async function prevMonth(){
     if(selectedTime.getMonth() == 0){
-        setSelectedTime(12 + "/" + selectedTime.getDate() + "/" + selectedTime.getFullYear())
+        selectedTime.setMonth(11)
+        selectedTime.setFullYear(selectedTime.getFullYear()-1)
     }else{
-        setSelectedTime(selectedTime.getMonth() + "/" + selectedTime.getDate() + "/" + selectedTime.getFullYear())
+        selectedTime.setMonth(selectedTime.getMonth()-1)
     }
+    selectedDayDetails()
 }
 document.getElementById('prevBtn').addEventListener('click',() => {
     prevMonth()
 })
-
-
-setSelectedTime()
 
 function addClickEventToEachDay(){
     allDayNodes = document.querySelectorAll('.box')
     allDayNodes.forEach((element, index)=> {
         element.addEventListener('click', () => {
             toggleShow()
-            let temp = selectedTime.getMonth() + `/${index+1}/` + selectedTime.getFullYear()
-            reserveSelected = temp
-            setSelectedTime(temp, false)
+            let timeTemp = `${selectedTime.getMonth()+1}` + `/${index+1}/` + selectedTime.getFullYear()
+            reserveSelected = timeTemp
+            setSelectedTime(timeTemp, false)
+
+            //replace available value to text area or ""
+            eventArray.forEach(x => {
+                if(x.date === timeTemp){
+                    textArea.value = x.text
+                }else{
+                    textArea.value = ''
+                }
+            })
         })
     })
 }
@@ -124,3 +169,6 @@ document.getElementById('saveEvent').addEventListener('click',() => {
     setSelectedTime(reserveSelected)
     toggleShow()
 })
+
+
+setSelectedTime()
